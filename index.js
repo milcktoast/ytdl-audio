2e6bf879ded56ac3cf1fa70ddc563a833f9f4f87
Jay Weeks <jay.patrick.weeks@gmail.com>
2016-10-10 14:38:58 -0700

    Update seek arg to use yt time format

--- index.js
+++ index.js
@@ -19,11 +19,25 @@ var writer = ffmpeg(reader)
   .format(args.format)
   .audioBitrate(args.bitrate)
 
-if (args.seek) writer.seekInput(args.seek)
+if (args.seek) writer.seekInput(formatTime(args.seek))
 if (args.duration) writer.duration(args.duration)
 
 writer.output(process.stdout).run()
 
 function formatUrl (id) {
   return 'https://www.youtube.com/watch?v=' + id
+}
+
+function formatTime (time) {
+  var minutes = time.match(/(\d+)m/)
+  var seconds = time.match(/(\d+)s/)
+  return [
+    minutes.length && padLeft(minutes[1], 2),
+    seconds.length && padLeft(seconds[1], 2)
+  ].filter(Boolean).join(':')
+}
+
+function padLeft (str, length) {
+  while (str.length < length) str = '0' + str
+  return str
 }
